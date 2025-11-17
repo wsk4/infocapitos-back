bd48a459fea0e7de48dee4014c9172058760d43e
wsk4 <maa.gonzalezva@duocuc.cl>
2025-11-17 19:42:16 -0300

    creacion

---
+++ src/main/java/com/back/infocapitos_back/InfocapitosBackApplication.java
@@ -0,0 +1,13 @@
+package com.back.infocapitos_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfocapitosBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfocapitosBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/back/infocapitos_back/InfocapitosBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back.infocapitos_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfocapitosBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
